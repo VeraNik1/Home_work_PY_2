fef728523b2067652bf28c98f5e8b31d4a3e2f3c
Vera <solnce288888@gmail.com>
2022-12-15 23:32:50 +0300

    num 12

--- h_w2.py
+++ h_w2.py
@@ -9,18 +9,18 @@
 # 2
 
 
-import random as r
-while True:
-    try:
-        n = int(input('Введите количество монет: '))
-        if n <= 0:
-            raise Exception
-        coins = [r.randrange(0, 2) for i in range(n)]
-        print('выпали такие монетки:', *coins)
-        print(f'минимальное количество монет, которое нужно перевернуть: {min(coins.count(1), coins.count(0))}')
-        break
-    except:
-        print("Вы ввели некорректное значение, попробуйте еще раз")
+# import random as r
+# while True:
+#     try:
+#         n = int(input('Введите количество монет: '))
+#         if n <= 0:
+#             raise Exception
+#         coins = [r.randrange(0, 2) for i in range(n)]
+#         print('выпали такие монетки:', *coins)
+#         print(f'минимальное количество монет, которое нужно перевернуть: {min(coins.count(1), coins.count(0))}')
+#         break
+#     except:
+#         print("Вы ввели некорректное значение, попробуйте еще раз")
 
 
 
@@ -30,6 +30,36 @@ while True:
 #  Он задумывает два натуральных числа X и Y (X,Y≤1000), а Катя должна их отгадать. 
 # Для этого Петя делает две подсказки. Он называет сумму этих чисел S и их произведение P. 
 # Помогите Кате отгадать задуманные Петей числа.
+
+
+
+while True:
+    try:
+        S = int(input('Введите сумму натуральных чисел X и Y: '))
+        P = int(input('Введите произведение натуральных чисел X и Y: '))
+
+        if P <= 0 or S <= 0:
+            raise Exception
+        x = 0
+        y = 0
+        for i in range(1, P//2 + 1):
+            if P == (S - i) * i:
+                x = i
+                y = S - i
+                break
+                
+        if x:
+            print(f'Петя загадал X = {x} Y = {y}')
+        else:
+            print('Петя не умеет считать!')
+        break
+    except:
+        print("Вы ввели некорректное значение суммы или произведения, попробуйте еще раз")
+
+
+
+
+
 
 
 # Задача 14
